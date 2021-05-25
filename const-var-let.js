906c34e59c07a4bb5c8961477c14d95404c08833
Cristofin <j.c.luque@hotmail.com>
2021-05-25 18:35:19 +0200

    Initial commit

---
+++ const-var-let.js
@@ -0,0 +1,11 @@
+var nombre = 'Ironman'; // Variable global
+
+if (true) {
+
+    let nombre = 'Thor'; // Variable del scope
+
+    console.log(nombre);
+
+}
+
+console.log(nombre);
---
+++ template-string.js
@@ -0,0 +1,15 @@
+const nombre = 'Ironman';
+const real = 'Stark';
+
+const normal = nombre + ' ' + real;
+const template = `El apellido de ${nombre} es ${real}`;
+
+console.log(normal);
+console.log(template);
+
+const html = `
+    <h1>Hola</h1>
+    <p>mundo</p>
+`;
+
+console.log(html);
